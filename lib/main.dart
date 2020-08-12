4c61e6646d8b8a8eae2538f669190f841cc77c38
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-08-12 10:50:14 +0100

    dynamic images on click

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text(
+            'Dicee',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
+  void updateDice() {
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                updateDice();
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNum.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              autofocus: true,
+              onPressed: () {
+                updateDice();
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNum.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
